8ee7bded9dd10257b1109038514175f151bb16ef
Rafael Lopes <rafael.mediario@gmail.com>
2015-06-02 07:01:05 -0300

    Add redcarpet

--- app/assets/javascripts/pad.js
+++ app/assets/javascripts/pad.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
 
   var delay;
-  var $form   = $("form");
-  var $submit = $("form input[type='submit']");
+  var $form       = $("form");
+  var $submit     = $("form input[type='submit']");
+  var $mark_down  = $("#mark_down");
+  var $pad_text   = $(".pad_text");
 
   $submit.hide();
+  $pad_text.focus();
+
+  $mark_down.on("click", function(){
+    var pad_text = $pad_text.html();
+    $mark_down.hide();
+    $pad_text.focus().html("").html(pad_text);
+  });
 
   $("textarea").on("keypress change",function(){
-      
+    
+    $mark_down.fadeOut(200);
     clearTimeout(delay);
 
     delay = setTimeout(function(){
       $form.submit();
-    },2000);  
+    },2000); 
   });
 
   $form.submit(function(){
@@ -23,10 +33,14 @@ $(document).ready(function(){
       data: fields,
       method: 'post',
       success: function(data){
-        console.log('save',data);
+        // TODO
       }
     });
 
     return false;
+  });
+
+  $( window ).unload(function() {
+    $form.submit();
   });
 });
